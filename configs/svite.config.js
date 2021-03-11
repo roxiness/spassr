144c88a30a4f43d4c7ed84a6f56992de41e6b48a
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-03-11 09:25:01 +0100

    fix: svite

--- configs/svite.config.js
+++ configs/svite.config.js
@@ -3,15 +3,23 @@ module.exports = {
     condition: ({ pkgjson }) => pkgjson.dependencies['@svitejs/vite-plugin-svelte'],
     supersedes: ['default'],
     config: () => {
-        const { readFileSync } = require('fs')
-        const html = readFileSync('./dist/index.html', 'utf8')
-        const script = 'dist' + html.match(/<script .+?src="([^"]+)"/)[1]
-
-        return {
+        const config = {
             assetsDir: 'dist',
             entrypoint: 'dist/index.html',
-            ssrOptions: { inlineDynamicImports: true },            
-            script
+            ssrOptions: { inlineDynamicImports: true }
         }
+
+        const script = getScript(config.entrypoint)
+        if (script)
+            config.script = `dist${script}`
+
+        return config
     }
+}
+
+function getScript(entrypoint) {
+    const { readFileSync, existsSync } = require('fs')
+    if (existsSync(entrypoint))
+        return readFileSync(entrypoint, 'utf8')
+            .match(/<script .+?src="([^"]+)"/)[1]
 }
